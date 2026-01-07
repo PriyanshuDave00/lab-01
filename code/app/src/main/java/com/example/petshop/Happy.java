19b3466389925cf866a942df0999e72c92491aa5
PriyanshuDave00 <pndave@ualberta.com>
2026-01-07 11:44:09 -0700

    latest

---
+++ code/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,16 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood {
+
+    // Default constructor
+    public Happy() {
+        super();
+    }
+
+    // Constructor with date
+    public Happy(Date date) {
+        super(date);
+    }
+}
---
+++ code/app/src/main/java/com/example/petshop/Mood.java
@@ -0,0 +1,27 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public abstract class Mood {
+    protected Date date;
+
+    // Constructor that sets date to default (current date)
+    public Mood() {
+        this.date = new Date();
+    }
+
+    // Constructor that takes a date as an argument
+    public Mood(Date date) {
+        this.date = date;
+    }
+
+    // Getter
+    public Date getDate() {
+        return date;
+    }
+
+    // Setter
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
